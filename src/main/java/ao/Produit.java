702419af8b808f6cae41109ccf9a0f1b60917459
Kirsan <saito-@kik@hotmail.com>
2020-10-02 14:33:04 +0200

    ajout méthode equals hashcode et tostring pour les values object

--- src/main/java/ao/Produit.java
+++ src/main/java/ao/Produit.java
@@ -2,13 +2,15 @@
 package main.java.ao;
 
 import java.util.*;
+import java.lang.*;
+import java.security.InvalidParameterException;
 
 public class Produit {
-   
-    private Reference ref;
-    private Name nom;
-    private Description description;
-    private Price prix;
+    
+    private final Reference ref;
+    private final Name nom;
+    private final Description description;
+    private final Price prix;
 
     public Reference getRef() {
         return ref;
@@ -54,41 +56,72 @@ public class Produit {
 
 // ValueObject
 class Name{
-    private String nom;
+    private final String nom;
 
-    public Name(String nom){
-        if(nom.length()>20){
-            System.err.println("Nom supérieur à 20 caractère\n");
+    public Name(String nom) throws InvalidParameterException {
+        if( nom==null || nom.length()>20){
+            throw new InvalidParameterException();
         } 
         else{
             this.nom = nom; 
         }
     }
 
+    public boolean equals(Object obj) {
+        if( !(obj instanceof Name)) return false;
+        Name otherName = (Name) obj;
+        boolean sameName = this.nom == otherName.nom;
+        return sameName;
+    }
+
+    public int hashCode() {
+        return Objects.hash(nom);
+    }
+
+    public String toString(){
+        return "Name: "+name+super.toString();
+    }
 }
 
+//
 class Description{
-    private String description;
+    private final String description;
 
-    public Description(String description){
-        if(description.length()>200){
-            System.err.println("Description supérieur à 200 caractère\n");
+    public Description(String description) throws InvalidParameterException{
+        if(description==null || description.length()>200){
+            throw new InvalidParameterException();
         }
         else{
             this.description = description;    
         }
     }
+
+    public int hashCode() {
+        return Objects.hash(description);
+    }
+
+    public boolean equals(Object obj) {
+        if( !(obj instanceof Description)) return false;
+        Description otherDescription = (Description) obj;
+        boolean samedescription = this.description == otherDescription.description;
+        return samedescription;
+    }
+
+    public String toString(){
+        return "Description: "+description+super.toString();
+    }
 }
 
+//
 class Reference{
-    private String ref;
+    private final String ref;
 
-    public Reference(String ref){
-        if (isAlphanumeric(ref) == true && ref.length()<=20){
+    public Reference(String ref) throws InvalidParameterException{
+        if (isAlphanumeric(ref) == true && ref.length()<=20 && ref!=null){
             this.ref = ref;
         }
         else{
-            System.err.println("Reference doit être alphanumérique de taille 20 au maximum\n");
+            throw new InvalidParameterException();
         }
     }
 
@@ -102,17 +135,48 @@ class Reference{
        }
        return true;
     }
+
+    public int hashCode() {
+        return Objects.hash (ref);
+    }
+
+    public boolean equals(Object obj) {
+        if( !(obj instanceof Reference)) return false;
+        Reference otherReference = (Reference) obj;
+        boolean sameReference = this.ref == otherReference.ref;
+        return sameReference;
+    }
+
+    public String toString(){
+        return "Reference: "+ref+super.toString();
+    }
 }
 
+//
 class Price{
-    private int prix;
+    private final int prix;
 
-    public Price(int prix){
+    public Price(int prix) throws InvalidParameterException{
         if(prix>0){
             this.prix=prix;
         }
         else{
-            System.err.println("Prix doit être un entier positif\n");
+            throw new InvalidParameterException();
         }
+    }
+
+    public boolean equals(Object obj) {
+        if( !(obj instanceof Price)) return false;
+        Price otherPrice = (Price) obj;
+        boolean samePrice = this.prix == otherPrice.prix;
+        return samePrice;
+    }
+
+    public int hashCode() {
+        return Objects.hash(prix);
+    }
+
+    public String toString(){
+        return "Price: "+prix+super.toString();
     }
 }
