0bb51c5db50d1e81fc9613c81c283f0fed8d9bd1
Kirsan <46657440+Kirsan98@users.noreply.github.com>
2020-10-02 09:22:54 +0200

    Add files via upload

---
+++ Produit.java
@@ -0,0 +1,93 @@
+// ValueObject
+public class Produit {
+   
+    private Reference ref;
+    private Name nom;
+    private Description description;
+    private Price prix;
+
+    public Reference getRef() {
+        return ref;
+    }
+
+    public Name getNom() {
+        return nom;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    public Price getPrix() {
+        return prix;
+    }
+
+    public void Reference() {
+    }
+
+    public void Reference(Reference ref, Name nom, Description description, Price prix) {
+        this.ref = ref;
+        this.nom = nom;
+        this.description = description;
+        this.prix = prix; 
+    }
+
+    // TODO Surcharger la méthode equals et hashcode pour faire en sorte que l'égalité soit une égalité de valeur.
+}
+
+// ValueObject
+class Name{
+    private final String nom;
+
+    public Name(String nom){
+        if(nom.length()>20){
+            System.err.println("Nom supérieur à 20 caractère\n");
+        } 
+        this.nom = nom; 
+    }
+
+}
+
+class Description{
+    private final String description;
+
+    public Description(String description){
+        if(description.length()>200){
+            System.err.println("Description supérieur à 200 caractère\n");
+        }
+        this.description = description;
+    }
+}
+
+class Reference{
+    private final String ref;
+
+    public Reference(String ref){
+        if (isAlphanumeric(ref) == true && ref.length()<=20){
+            this.ref = ref;
+        }
+        System.err.println("Reference doit être alphanumérique de taille 20 au maximum\n");
+    }
+
+    public boolean isAlphanumeric(String str)
+    {
+       char[] charArray = str.toCharArray();
+       for(char c:charArray)
+       {
+           if (!Character.isLetterOrDigit(c))
+               return false;
+       }
+       return true;
+    }
+}
+
+class Price{
+    private final int prix;
+
+    public Price(int prix){
+        if(prix>0){
+            this.prix=prix;
+        }
+        System.err.println("Prix doit être un entier positif\n");
+    }
+}
